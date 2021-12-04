6ec1851de276a3998807f624a1984d717fb4ffa8
Andresito de Guzman <deguzman.andresito@gmail.com>
2021-12-04 00:44:02 +0000

    feat: feat: add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'your-way-coffee',
+    appId: '1:901169466456:web:bb0b1016e5c0f18efc4e2d',
+    storageBucket: 'your-way-coffee.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBGwzPKgeMRx_knLxBQew8QeQLRx7rezag',
+    authDomain: 'your-way-coffee.firebaseapp.com',
+    messagingSenderId: '901169466456',
+    measurementId: 'G-TYZRB30MJ7',
+  },
   production: true
 };
